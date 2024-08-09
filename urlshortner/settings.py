fd5a4c45836ddad3306f0c673f95ad756e2339d6
naturalneuralnet <natneuralnet@outlook.com>
2024-08-09 17:41:24 +0100

    updating

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
