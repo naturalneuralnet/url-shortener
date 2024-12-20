f0e85ab3248e7834a89c97a5637533a3bd243530
naturalneuralnet <natneuralnet@outlook.com>
2024-12-20 20:33:15 +0000

    adding env variables

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = "DEBUG"
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = ["localhost"]
 
 # Application definition
 
